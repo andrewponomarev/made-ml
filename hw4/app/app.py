23f2a7c01153878684f41fcbf34d16d5de3f916b
ponomarevandrew <Caf-Lw9-TV5-pku>
2020-12-27 04:19:46 +0300

    fixes

--- hw4/app/app.py
+++ hw4/app/app.py
@@ -166,4 +166,6 @@ def button():
 
 
 if __name__ == '__main__':
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
